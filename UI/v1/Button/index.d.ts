d83cd221547418da3376b282ca5cb97ed29bab8e
minhhhuongnguyenthi.st@gmail.com <jenny@islandpeakcloud.sg>
2022-05-28 16:45:01 +0700

    Build sdk

---
+++ UI/v1/Button/index.d.ts
@@ -0,0 +1,2 @@
+declare const CustomButton: import("@mui/material").ExtendButtonBase<import("@mui/material/Button").ButtonTypeMap<{}, "button">>;
+export default CustomButton;
---
+++ UI/v1/Button/index.js
@@ -0,0 +1,6 @@
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+var CustomButton = styled(Button)({
+  color: "#20b2aa"
+});
+export default CustomButton;
